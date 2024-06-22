08a7e64437716fa95842f18f6aebe910704548d4
amcleod72 <andrew@mcleods.net>
2024-06-22 19:11:58 +1000

    Added comments to app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const path = require ('path')
 
+// Home page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,`/public/index.html`));
 })
