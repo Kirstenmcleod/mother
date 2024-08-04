ffd53baa407b9205fd5d2b57bc751e32f555e019
amcleod72 <andrew@mcleods.net>
2024-08-04 13:29:01 +1000

    Addition of serverless

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const serverless    = require("serverless-http");
 const express       = require('express');
-let compression       = require('compression');
-var cookieSession     = require('cookie-session');
-const cookieParser    = require('cookie-parser');
-const bodyParser      = require('body-parser');
 const app           = express();
-const fs            = require("fs");
 const path          = require('path');
 const locations     = require('./ajax/locations');
 const secrets       = require('./lib/secrets');
@@ -14,25 +9,6 @@ const secrets       = require('./lib/secrets');
 require('dotenv').config();
 
 // Configure Express
-app.use(cookieParser());
-app.disable('x-powered-by');
-app.enable('trust proxy');
-app.set('trust proxy', 10);
-app.use(compression());
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(bodyParser.raw({type: 'application/jwt'}));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieSession({
-    secret: 'aedc75f7-90b9-47ad-a5d4-2b10d62a9d85',
-    name: '_jb',
-    keys: ["main"],
-    secure: true,
-    httpOnly: true,
-    sameSite: 'none',
-    maxAge: 30 * 60 * 1000
-}));
-
 app.use(async function(req, res, next) {
     console.log('__dirname',__dirname)
     console.log(`${req.method} - ${req.url}`);
